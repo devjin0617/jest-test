378bb65082b9ab6ec7df11a1e1468bc2b0261e15
devjin0617 <devjin0617@gmail.com>
2018-01-25 16:15:10 +0900

    공부중 ~_~

--- sum.test.js
+++ sum.test.js
@@ -3,3 +3,13 @@ const sum = require('./sum')
 test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3)
 })
+
+test('two plus two is four', () => {
+  expect(2 + 2).toBe(4)
+})
+
+test('object assignment', () => {
+  const data = {one: 1}
+  data['two'] = 2
+  expect(data).toEqual({one: 1, two: 2})
+})
