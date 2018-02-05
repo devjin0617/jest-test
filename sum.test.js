2e4197dcbce1d7bebd5c0bda63d93f5b61372054
devjin0617 <devjin0617@gmail.com>
2018-02-05 16:12:31 +0900

    Add null and zero test

--- sum.test.js
+++ sum.test.js
@@ -21,3 +21,29 @@ test('adding positive numbers is not zero', () => {
     }
   }
 })
+
+/*
+  toBeNull: null인 경우 통과
+  toBeUndefined: undefined인 경우 통과
+  toBeDefined: undefined의 반대상황일 경우 통과
+  toBeTruthy: 값이 if조건에 따라 true일 경우 통과
+  toBeFalsy: 값이 if조건에 따라 false일 경우 통과
+*/
+
+test('null', () => {
+  const n = null
+  expect(n).toBeNull()
+  expect(n).toBeDefined()
+  expect(n).not.toBeUndefined()
+  expect(n).not.toBeTruthy()
+  expect(n).toBeFalsy()
+})
+
+test('zero', () => {
+  const z = 0
+  expect(z).not.toBeNull()
+  expect(z).toBeDefined()
+  expect(z).not.toBeUndefined()
+  expect(z).not.toBeTruthy()
+  expect(z).toBeFalsy()
+})
