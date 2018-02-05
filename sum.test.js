68fc8db18363ccfb280e287d8d770f72877ef63f
devjin0617 <devjin0617@gmail.com>
2018-02-05 16:06:57 +0900

    adding positive numbers is not zero

--- sum.test.js
+++ sum.test.js
@@ -13,3 +13,11 @@ test('object assignment', () => {
   data['two'] = 2
   expect(data).toEqual({one: 1, two: 2})
 })
+
+test('adding positive numbers is not zero', () => {
+  for (let a = 1; a < 10; a++) {
+    for (let b = 1; b < 10; b++) {
+      expect(a + b).not.toBe(0)
+    }
+  }
+})
